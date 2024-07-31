c12f6c57611274ed3ba2cfba8eeac8d5feb8f53a
ahmad <ahmad.oudai1999@gmail.com>
2024-07-31 23:06:32 +0200

    Setup Express Server With GraphQL

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const expressGraphQL = require('express-graphql');
+const app = express();
+
+app.use('/graphql', expressGraphQL({
+    graphiql: true
+}));
+
+app.list(5000, () => console.log('Server is running on port 5000'));
